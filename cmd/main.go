322684f38b188b7c24527d2e5e51dbd8773026a4
Lukas Jokubauskas <lukjok7@gmail.com>
2021-07-05 20:36:18 +0300

    Added basic CLI

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"time"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	// Hello world
-	log.Println(":)")
+	app := &cli.App{
+		Name:      "gRPCFuzz",
+		Version:   "0.1",
+		Compiled:  time.Now(),
+		HelpName:  "contrive",
+		Usage:     "a grey-box feedback-based gRPC fuzzer",
+		UsageText: "gipcfuzz [options] [arguments]",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "arg",
+				Aliases: []string{"a"},
+				Value:   "Test",
+				Usage:   "Sample argument",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			fmt.Printf("%#v\n", c.String("arg"))
+			if c.Bool("infinite") {
+				c.App.Run([]string{})
+			}
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
